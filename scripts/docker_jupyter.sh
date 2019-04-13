9b36d0ba7c917a02328e7df9b7fe6a6383654ed9
Albert Choy <albert.choy@osii.com>
2019-04-12 23:15:35 -0500

    fix errors

--- scripts/docker_jupyter.sh
+++ scripts/docker_jupyter.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 #docker run -it -p 8888:8888 -p 6006:6006 -v $PWD/sharedfolder:/root/sharedfolder \
 #  achoy/vision-talk:latest jupyter notebook --allow-root
 
 docker run -it -p 8888:8888 -p 6006:6006 -v $PWD/sharedfolder:/home/achoy/sharedfolder \
   achoy/vision-talk:latest3 jupyter notebook --ip=0.0.0.0 --allow-root
-=======
-docker run -it -p 8888:8888 -p 6006:6006 -v $PWD/sharedfolder:/root/sharedfolder \
-  achoy/vision-talk:latest jupyter notebook --allow-root
->>>>>>> a62b549c15903eb2679932bbccbdd9b862e050cb
